6b83f2415f4d79cf142d25fe3bf18938d697747f
Kubilay Hazır <kubi@basecode.al>
2022-10-28 01:17:35 +0200

    Version Update

--- bin/create.js
+++ bin/create.js
@@ -9,14 +9,14 @@ const create = async (args) => {
     if(!await fs.existsSync(project_name)) {
         shell.exec("git clone git@github.com:basecodeDev/Base.git " + project_name);
         shell.exec("cd " + project_name + " && rm -rf .git && git init");
-        shell.exec("cd " + project_name + " && mv config/index.sample.js config/index.js");
-        log.ok(project_name + ' project created successfully');
-        log.ok(project_name + '/config/index.js for configurations');
-        log.ok(project_name + '/app/modules for modules');
-        log.ok(project_name + '/app/tools for tools');
-        log.ok(project_name + '/app/migrations for custom migrations');
-        log.ok(project_name + '/app/tests for manual tests');
-        log.ok(project_name + '/app/install.json for third party modules');
+        shell.exec("cd " + project_name + " && mv app/config/index.sample.js app/config/index.js");
+        log.success(project_name + ' project created successfully');
+        log.info(project_name + '/app/config/index.js for configurations');
+        log.info(project_name + '/app/modules for modules');
+        log.info(project_name + '/app/tools for tools');
+        log.info(project_name + '/app/migrations for custom migrations');
+        log.info(project_name + '/app/tests for manual tests');
+        log.info(project_name + '/app/install.json for third party modules');
     } else {
         log.error(project_name + ' project already exists');
     }
