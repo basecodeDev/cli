59ca0abb636fa149a38592177fe0075a9ee3b03f
Kubilay Hazır <kubi@basecode.al>
2022-10-28 01:21:24 +0200

    Version Update

--- bin/create.js
+++ bin/create.js
@@ -7,9 +7,11 @@ const create = async (args) => {
     const project_name = args.name || 'construct';
 
     if(!await fs.existsSync(project_name)) {
+        shell.exec("npm install -g yarn");
         shell.exec("git clone git@github.com:basecodeDev/Base.git " + project_name);
         shell.exec("cd " + project_name + " && rm -rf .git && git init");
         shell.exec("cd " + project_name + " && mv app/config/index.sample.js app/config/index.js");
+        shell.exec("cd " + project_name + " && yarn");
         log.success(project_name + ' project created successfully');
         log.info(project_name + '/app/config/index.js for configurations');
         log.info(project_name + '/app/modules for modules');
