4a9793075a37f4df5074113294f0f932bea14f28
Kubilay Hazır <kubi@basecode.al>
2022-10-28 13:12:06 +0200

    Version Update

--- bin/package.js
+++ bin/package.js
@@ -2,18 +2,19 @@ const shell = require("shelljs");
 const log = require('log-beautify');
 const prompt = require("prompt-sync")({ sigint: true });
 const fs = require('fs');
+const pathNow = process.cwd()
 
 const add = async () => {
 
     const moduleName = prompt("Module name:");
     const repositories = prompt("Module repo url:");
     
-    if(await fs.existsSync('app/modules')) {
+    if(await fs.existsSync(pathNow + '/app/modules')) {
 
         let installedPackages = []
 
-        if(await fs.existsSync('app/install.json')) {
-            installedPackages = require('app/install.json');
+        if(await fs.existsSync(pathNow + '/app/install.json')) {
+            installedPackages = require(pathNow + '/app/install.json');
         }
 
         installedPackages.push({
@@ -21,59 +22,59 @@ const add = async () => {
             repo: repositories
         })
 
-        fs.writeFileSync('app/install.json', JSON.stringify(installedPackages, null, 4));
+        fs.writeFileSync(pathNow + '/app/install.json', JSON.stringify(installedPackages, null, 4));
 
-        if(!await fs.existsSync('app/modules/'+moduleName)) {
-            shell.exec("git clone "+repositories+" "+__dirname+"/app/modules/"+moduleName);
+        if(!await fs.existsSync(pathNow + '/app/modules/'+moduleName)) {
+            shell.exec("git clone "+repositories+" "+pathNow+"/app/modules/"+moduleName);
         }
 
         log.success('Module installed');
 
     } else {
-        log.error('app/modules directory not found');
+        log.error(pathNow + '/app/modules directory not found');
     }
 
 }
 
 const install = async (args) => {
 
-    if(await fs.existsSync('app/install.json')) {
+    if(await fs.existsSync(pathNow + '/app/install.json')) {
 
-        const installedPackages = require('app/install.json');
+        const installedPackages = require(pathNow + '/app/install.json');
 
         for (let i = 0; i < installedPackages.length; i++) {
             const element = installedPackages[i];
-            if(!await fs.existsSync('app/modules/'+element.name)) {
-                shell.exec("git clone "+element.repo+" "+__dirname+"/app/modules/"+element.name);
+            if(!await fs.existsSync(pathNow + '/app/modules/'+element.name)) {
+                shell.exec("git clone "+element.repo+" "+pathNow+"/app/modules/"+element.name);
             }
         }
 
         log.success('Modules installed');
 
     } else {
-        log.error('app/install.json file not found');
+        log.error(pathNow + '/app/install.json file not found');
     }
 
 }
 
 const update = async () => {
 
-    if(await fs.existsSync('app/install.json')) {
+    if(await fs.existsSync(pathNow + '/app/install.json')) {
 
-        const installedPackages = require('app/install.json');
+        const installedPackages = require(pathNow + '/app/install.json');
 
         for (let i = 0; i < installedPackages.length; i++) {
             const element = installedPackages[i];
 
-            if(await fs.existsSync('app/modules/'+element.name)) {
-                shell.exec("cd "+__dirname+"/app/modules/"+element.name+" && git pull");
+            if(await fs.existsSync(pathNow + '/app/modules/'+element.name)) {
+                shell.exec("cd "+pathNow+"/app/modules/"+element.name+" && git pull");
             }
         }
 
         log.success('Modules updated');
 
     } else {
-        log.error('app/install.json file not found');
+        log.error(pathNow + 'app/install.json file not found');
     }
 
 }
