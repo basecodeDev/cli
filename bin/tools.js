98334435124ac8bf8a2040dcee9ba1818775c372
Kubilay Hazır <kubi@basecode.al>
2022-11-04 00:50:10 +0100

    Version Update

--- bin/tools.js
+++ bin/tools.js
@@ -3,8 +3,8 @@ const log = require('log-beautify');
 const fs = require('fs');
 
 const create = async (args) => {
-    if(await fs.existsSync('node_modules/construct/bin/createModule.js')) {
-        shell.exec("node node_modules/construct/bin/createModule.js");
+    if(await fs.existsSync('node_modules/construct/bin/createTools.js')) {
+        shell.exec("node node_modules/construct/bin/createTools.js");
     } else {
         log.error('Construct is not installed');
     }
