adf6d6a76fc6355f758c773ed4d1329200174e78
Kubilay Hazır <kubi@basecode.al>
2022-12-08 16:56:20 +0100

    Login with user

--- bin/upload.js
+++ bin/upload.js
@@ -1,42 +1,53 @@
-const shell = require("shelljs");
 const log = require('log-beautify');
 const fs = require('fs');
 const archiver = require('archiver');
+const { login } = require('../lib/user')
 const pathNow = process.cwd()
 
 const upload = async (args = undefined) => {
 
-    const module_name = args || 'construct';
+    const user = await login();
 
-    const source_dir = pathNow + '/app/modules/' + module_name;
+    if(user) {
 
-    if(!await fs.existsSync(pathNow + '/dist')) {
-        await fs.mkdirSync(pathNow + '/dist');
-    }
-    
-    if(await fs.existsSync(source_dir)) {
-        const output = fs.createWriteStream(pathNow + '/dist/' + module_name + '.zip');
-        const archive = archiver('zip');
+        const module_name = args || 'construct';
 
-        output.on('close', function () {
-            console.log(archive.pointer() + ' total bytes');
-            console.log('archiver has been finalized and the output file descriptor has closed.');
-        });
+        const source_dir = pathNow + '/app/modules/' + module_name;
 
-        archive.on('error', function(err){
-            throw err;
-        });
+        if(!await fs.existsSync(pathNow + '/dist')) {
+            await fs.mkdirSync(pathNow + '/dist');
+        }
+        
+        if(await fs.existsSync(source_dir)) {
+            const output = fs.createWriteStream(pathNow + '/dist/' + module_name + '.zip');
+            const archive = archiver('zip');
 
-        archive.pipe(output);
+            output.on('close', function () {
+                console.log(archive.pointer() + ' total bytes');
+                console.log('archiver has been finalized and the output file descriptor has closed.');
+                
+                if(archive.pointer() > 0) {
+                    log.success('Module uploaded successfully');
+                }
+            });
 
-        // append files from a sub-directory, putting its contents at the root of archive
-        archive.directory(source_dir, false);
+            archive.on('error', function(err){
+                throw err;
+            });
 
-        archive.finalize();
+            archive.pipe(output);
 
-        log.success('Module uploaded successfully');
+            // append files from a sub-directory, putting its contents at the root of archive
+            archive.directory(source_dir, false);
+
+            archive.finalize();
+        } else {
+            log.error('Module not found');
+        }
+
     } else {
-        log.error('Module not found');
+        log.error('Login failed');
+        return;
     }
 }
 
