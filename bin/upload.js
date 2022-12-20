2a55942415348db215eb4d68f398b7e9fb431797
Kubilay Hazır <kubi@basecode.al>
2022-12-20 16:06:57 +0100

    Version Update

--- bin/upload.js
+++ bin/upload.js
@@ -1,8 +1,11 @@
 const log = require('log-beautify');
 const fs = require('fs');
+const axios = require('axios');
 const archiver = require('archiver');
 const { login } = require('../lib/user')
+const FormData = require('form-data');
 const pathNow = process.cwd()
+const baseUrl = 'https://dev.base.al/api/v1/'
 
 const upload = async (args = undefined) => {
 
@@ -13,6 +16,7 @@ const upload = async (args = undefined) => {
         const module_name = args || 'construct';
 
         const source_dir = pathNow + '/app/modules/' + module_name;
+        // const source_dir = pathNow + '/bin'; // For testing
 
         if(!await fs.existsSync(pathNow + '/dist')) {
             await fs.mkdirSync(pathNow + '/dist');
@@ -27,6 +31,7 @@ const upload = async (args = undefined) => {
                 console.log('archiver has been finalized and the output file descriptor has closed.');
                 
                 if(archive.pointer() > 0) {
+                    uploadPackage(pathNow + '/dist/' + module_name + '.zip', module_name, user)
                     log.success('Module uploaded successfully');
                 }
             });
@@ -48,6 +53,30 @@ const upload = async (args = undefined) => {
     } else {
         log.error('Login failed');
         return;
+    }
+}
+
+const uploadPackage = async (path, module_name, user = {}) => {
+
+    const form = new FormData();
+    const config = {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'token': user.token
+        }
+    }
+
+    form.append('name', module_name);
+    form.append('slug', module_name);
+    form.append('package', fs.createReadStream(path));
+
+    const { data } = await axios.post(baseUrl + 'packages/add', form, config);
+
+    if(data.status) {
+        log.success('Package has been uploaded successfully');
+    } else {
+        log.error('Package upload failed');
+        log.error(data.messages.join(','))
     }
 }
 
